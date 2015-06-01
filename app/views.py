21cf0afce74f6bcd4c72bc5f33154b7a1558fb9f
sasubillis <sasubillis@gmail.com>
2015-06-01 11:16:06 +0300

    added edit and delete routes

--- app/views.py
+++ app/views.py
@@ -11,3 +11,28 @@ def add():
         flash('New entry was successfully posted')     
              
     return render_template('add.html')
+
+@app.route('/' )
+def index():
+  post = Post.query.all()    
+  return render_template('index.html', post=post)
+
+@app.route('/edit/<id>' , methods=['POST', 'GET'])
+def edit (id):
+    #Getting user by primary key:
+    post = Post.query.get(id)
+    if request.method == 'POST':		
+		post.title = request.form['title']
+		post.text =  request.form['body']
+		db.session.commit()
+		return  redirect(url_for('index'))
+    return render_template('edit.html', post=post)
+ 
+@app.route('/delete/<id>' , methods=['POST', 'GET'])
+def delete (id):
+     post = Post.query.get(id)
+     db.session.delete(post)
+     db.session.commit()
+     flash ('deleted')
+	   
+     return redirect(url_for('index'))
